676dc4e0d0bf1c2d70c57ba17b10913a3d87e716
JAROSLAVS KROTS (2210273) <j.krots@rgu.ac.uk>
2025-09-18 15:34:39 +0100

    Removed classes from the assessment. Test functionality implementation.

--- assessment/main.js
+++ assessment/main.js
@@ -16,8 +16,33 @@ for (let i = 0; i<quiz.length; i++) {
     console.log(question);
     let str = "";
     for (let option of options) {
-        str += " " + option;
+        str += " " + option.to;
     }
     console.log(str);
     console.log(answer);
 }
+
+
+// const fs = require("node:fs");
+
+// let getDirs = function(rootDir, cb) { 
+//     fs.readdir(rootDir, function(err, files) { 
+//         let dirs = []; 
+//         for (let index = 0; index < files.length; ++index) { 
+//             let file = files[index]; 
+//             if (file[0] !== '.') { 
+//                 let filePath = rootDir + '/' + file; 
+//                 fs.stat(filePath, function(err, stat) {
+//                     if (stat.isDirectory()) { 
+//                         dirs.push(this.file); 
+//                     } 
+//                     if (files.length === (this.index + 1)) { 
+//                         return 0; 
+//                     } 
+//                 }.bind({index: index, file: file})); 
+//             }
+//         }
+//     });
+// }
+
+// console.log(getDirs("/"));
---
+++ day3/main.js
@@ -0,0 +1,6 @@
+let array = ["Test1", "Test2"];
+
+array.unshift("Test3");
+array.push("Test4");
+
+console.log(array);
